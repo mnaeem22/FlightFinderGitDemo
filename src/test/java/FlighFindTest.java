4b15b8419713d27a48f97f9d8691b281b1222511
mnaeem <mnaeem@gmail.com>
2024-05-18 16:43:55 +0500

    FlighFindTest is added

--- src/test/java/FlighFindTest.java
+++ src/test/java/FlighFindTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -23,7 +24,46 @@ public class FlighFindTest {
 
     }
 
-    @AfterMethod
+    @Test
+    public void searchFligh(){
+
+        WebElement flightOptionOneWayRB = driver.findElement(By.xpath("//input[@value='oneway']"));
+        flightOptionOneWayRB.click();
+
+        Select passengers = new Select(driver.findElement(By.name("passCount")));
+        passengers.selectByValue("2");
+
+        Select departingFrom = new Select(driver.findElement(By.name("fromPort")));
+        departingFrom.selectByValue("Frankfurt");
+
+        Select fromMonth = new Select(driver.findElement(By.name("fromMonth")));
+        fromMonth.selectByIndex(8);
+
+        Select fromDay = new Select(driver.findElement(By.name("fromDay")));
+        fromDay.selectByIndex(10);
+
+        Select arrivingIn = new Select(driver.findElement(By.name("toPort")));
+        arrivingIn.selectByValue("Paris");
+
+        Select toMonth = new Select(driver.findElement(By.name("toMonth")));
+        toMonth.selectByIndex(9);
+
+        Select toDay = new Select(driver.findElement(By.name("toDay")));
+        toDay.selectByIndex(10);
+
+        WebElement serviceClass = driver.findElement(By.xpath("//input[@value='Business']"));
+        serviceClass.click();
+
+        Select airline = new Select(driver.findElement(By.name("airline")));
+        airline.selectByVisibleText("Blue Skies Airlines");
+
+        WebElement continueButton = driver.findElement(By.xpath("//input[@name='findFlights']"));
+        continueButton.click();
+
+
+    }
+
+   @AfterMethod
     public void closeBrowser(){
         driver.quit();
     }
